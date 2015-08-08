aa7512ee3fa9caf012713b88aacb5a01701806b6
jasonzhou1 <jz629@cornell.edu>
2015-08-08 01:06:21 -0400

    Added html files into front end

---
+++ pdpsi/static/js/brothers.js
@@ -0,0 +1,34 @@
+
+
+//var classNames = ["Psi","Alpha Alpha", "Alpha Beta", "Alpha Gamma", "Alpha Delta"]
+
+
+document.getElementByID("anal").innerHTML = "hello world bitches"
+
+/**
+var panelGroup = document.createElement("div")
+panelGroup.class = "panel-group"
+panelGroup.id = "accordion"
+
+var panelHeading = document.createElement("div")
+panelHeading.class = "panel heading"
+
+panelGroup.appendChild(panelHeading)
+
+
+for (theClass in classNames){
+    
+    var newHeading = document.createElement("h4")
+    newHeading.class="text-center"
+    
+    var classLabel = document.createElement("a")
+    classLabel.href = "#collapseone"
+    
+    classLabel.innerHTML = theClass
+    
+    newHeading.appendChild(classLabel)
+    panelGroup.appendChild(newHeading)
+    
+}
+
+*/
